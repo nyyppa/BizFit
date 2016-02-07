28c789bc4ac714d0a588259019de1db459b517b2
kaupunki-apina <tomy_salminen@msn.com>
2016-02-07 12:42:41 +0200

    - Floating action button now disappears when scrolling down. The animation needs to be more graceful.

--- app/src/main/java/com/bizfit/bizfit/activities/MainActivity.java
+++ app/src/main/java/com/bizfit/bizfit/activities/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Space;
@@ -62,33 +63,32 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        /**
+         final TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+         tabLayout.setVisibility(View.VISIBLE);
+         tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+         tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+         tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        final PagerAdapter adapter = new PagerAdapter
-                (getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
+         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+         final PagerAdapter adapter = new PagerAdapter
+         (getSupportFragmentManager(), tabLayout.getTabCount());
+         viewPager.setAdapter(adapter);
+         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        @Override public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+        }
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
+        @Override public void onTabUnselected(TabLayout.Tab tab) {
 
-            }
+        }
 
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
+        @Override public void onTabReselected(TabLayout.Tab tab) {
 
-            }
-        });
+        }
+        }); */
     }
 
     protected void onStart() {
