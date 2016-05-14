33dbee9cd0727e03e5de809c9ff5a70d62379995
nyyppa <atte.yliverronen@gmail.com>
2016-05-14 12:50:19 +0300

    -added some commenting

--- app/src/main/java/com/bizfit/bizfit/DailyProgress.java
+++ app/src/main/java/com/bizfit/bizfit/DailyProgress.java
@@ -14,10 +14,18 @@ import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.ListIterator;
 
-
+/**
+ * Contains users progress in single tracking period
+ */
 public class DailyProgress implements java.io.Serializable {
     private List<DayPool> dayPool = new ArrayList<DayPool>(0);
     int id;
+
+    /**
+     * Constructs new DailyProgress from given list of daySingles and DailyProgress ID
+     * @param list List containing DaySingles that DailyProgress will hold
+     * @param id    ID of DailyProgress
+     */
     public DailyProgress(List<DaySingle> list, int id) {
         Comparator<DaySingle> comparator = new Comparator<DaySingle>() {
             @Override
@@ -38,15 +46,27 @@ public class DailyProgress implements java.io.Serializable {
 
     }
 
-
+    /**
+     * Default constructor
+     */
     public DailyProgress() {
 
     }
 
+    /**
+     * Creates DaySingle from given information and returns it without saving it to anywhere
+     * @param time
+     * @param amount
+     * @return
+     */
     public DaySingle createDaySingle(long time, float amount) {
         return new DaySingle(time, amount);
     }
 
+    /**
+     * Creates DailyProgress from given JSON
+     * @param jsonObject JSON containing nessessary information
+     */
     public DailyProgress(JSONObject jsonObject) {
         try {
             JSONArray jsonArray=jsonObject.getJSONArray("DaySingle");
@@ -61,6 +81,10 @@ public class DailyProgress implements java.io.Serializable {
         }
     }
 
+    /**
+     * Converts DailyProgress to JSON
+     * @return  JSON containing all information from DailyProgress
+     */
     public JSONObject toJSon(){
         JSONObject jsonObject=new JSONObject();
         JSONArray jsonArray=new JSONArray();
@@ -82,6 +106,10 @@ public class DailyProgress implements java.io.Serializable {
     }
 
 
+    /**
+     * Converts DailyProgress to easier saving to SQL based database
+     * @return List containing all DaySingles in this DailyProgress sorted for creation time
+     */
     public List<DaySingle> prepForDataBase() {
         List<DaySingle> list = new ArrayList<DaySingle>();
         for (int i = 0; i < dayPool.size(); i++) {
@@ -100,10 +128,20 @@ public class DailyProgress implements java.io.Serializable {
         return list;
     }
 
+    /**
+     *
+     * @return DayPool list containing users added progress
+     */
     public List<DayPool> getDayPoolList() {
         return dayPool;
     }
 
+    /**
+     * Adds progress for user and returns daySingle that was created as the result
+     * @param amount    How much user addded progress
+     * @param time  When user added progress
+     * @return  DaySingle created from given information
+     */
     public DaySingle addDailyProgress(float amount, long time) {
         if (dayPool.size() == 0) {
             dayPool.add(new DayPool(amount, time));
@@ -115,6 +153,9 @@ public class DailyProgress implements java.io.Serializable {
         return dayPool.get(dayPool.size() - 1).getLast();
     }
 
+    /**
+     * undoes last time user added progress
+     */
     public void undoLast() {
         if (dayPool.size() != 0) {
             boolean wasLast = dayPool.get(dayPool.size() - 1).removeLast();
@@ -124,6 +165,10 @@ public class DailyProgress implements java.io.Serializable {
         }
     }
 
+
+    /**
+     * removes lastDayPool from DailyProgress
+     */
     private void removeLast() {
         ListIterator<DayPool> iterator = dayPool.listIterator();
         while (iterator.hasNext()) {
@@ -133,38 +178,77 @@ public class DailyProgress implements java.io.Serializable {
     }
 
 
+    /**
+     * Container class for daySingle's
+     *
+     * Holds single days user added progress
+     */
     public class DayPool implements java.io.Serializable {
-        float TotalAmount;
+
         long time;
         List<DaySingle> daySingle = new ArrayList<DaySingle>(0);
 
+        /**
+         * Creates new DayPool and adds first daySingle with given information
+         * @param amount    amount user added progress
+         * @param time  time user added progress
+         */
         DayPool(float amount, long time) {
             this.time = time;
-            TotalAmount += amount;
             daySingle.add(new DaySingle(time,amount));
         }
 
+        /**
+         * returns last created DaySingle
+         * @return
+         */
         private DaySingle getLast(){
             return daySingle.get(daySingle.size()-1);
         }
 
+        /**
+         * adds new DaySingle from given information to DayPool
+         * @param amount    amount user added progress
+         * @param time  time when user added progress
+         */
         public void addDaySingle(float amount, long time) {
             daySingle.add(new DaySingle(time, amount));
-            TotalAmount += amount;
+  ;
         }
 
+        /**
+         * Converts DayPools creation time to OurDateTime
+         * @return OurDateTime made from DayPools creationTime
+         */
         public OurDateTime getDayTime() {
             return new OurDateTime(time);
         }
 
+        /**
+         *
+         * @return  Total amount of users added progress during single day for single tracker
+         */
         public float getTotalAmount() {
+            float TotalAmount=0;
+            for(int i=0;i<daySingle.size();i++){
+                TotalAmount+=daySingle.get(i).getAmount();
+            }
             return TotalAmount;
         }
 
+        /**
+         *
+         * @return Time when DayPool was created
+         */
         public long getTime() {
             return time;
         }
 
+        /**
+         * Checks if given time's day of month is same as time's held in DayPool
+         * @param timeTime  Time to Compare
+         * @return  if day of month is same returns true, else false
+         */
         private boolean sameDate(long timeTime) {
             GregorianCalendar ca = new GregorianCalendar();
             ca.setTimeInMillis(time);
@@ -177,13 +261,16 @@ public class DailyProgress implements java.io.Serializable {
             }
         }
 
+        /**
+         * Removes last instance of user added progress
+         * @return  if it was last instance in this DayPool
+         */
         public boolean removeLast() {
             ListIterator<DaySingle> iterator = daySingle.listIterator();
             DaySingle a = null;
             while (iterator.hasNext()) {
                 a = iterator.next();
             }
-            TotalAmount -= a.getAmount();
             iterator.remove();
             if (daySingle.size() == 0) {
                 return true;
@@ -193,12 +280,19 @@ public class DailyProgress implements java.io.Serializable {
         }
     }
 
-
+    /**
+     * Holds single instance of users added progress
+     */
     public class DaySingle implements java.io.Serializable {
         private long time;
         private float amount;
         public int id = -1;
 
+        /**
+         *
+         * @param Time  Creation time
+         * @param amount    Amount the user added progress
+         */
         DaySingle(long Time, float amount) {
             // TODO Auto-generated constructor stub
             this.time = Time;
@@ -206,14 +300,26 @@ public class DailyProgress implements java.io.Serializable {
             //id=
         }
 
+        /**
+         * Retuns creation time of DaySingle
+         * @return  Creation Time
+         */
         public long getTime() {
             return time;
         }
 
+        /**
+         * Retuns amount held in this DaySingle
+         * @return  Amount held in this DaySingle
+         */
         public float getAmount() {
             return amount;
         }
 
+        /**
+         * Converts DaySingle to JSON
+         * @return  JSON containing all of daySingles information
+         */
         private JSONObject ToJson(){
             JSONObject jsonObject=new JSONObject();
             try {
