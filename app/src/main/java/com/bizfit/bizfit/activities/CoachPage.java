f4c25a0002e3578db54af103bd3a412e152a2e83
JJarvenpaa <jari.jarvenpaa@hotmail.com>
2016-11-25 15:36:03 +0200

    Jjarvenpaa 25.11.16
    activities -> CoachPage.java: Deprecated function fixed.
    layout -> list_item_store_item.xml: Picture size reduced and new textView added.

--- app/src/main/java/com/bizfit/bizfit/activities/CoachPage.java
+++ app/src/main/java/com/bizfit/bizfit/activities/CoachPage.java
@@ -1,7 +1,11 @@
 package com.bizfit.bizfit.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
@@ -13,14 +17,16 @@ import com.ms.square.android.expandabletextview.ExpandableTextView;
 /**
  * Fetches and displays an overview of hireable coaches.
  */
-public class CoachPage extends AppCompatActivity {
+public class CoachPage extends AppCompatActivity
+{
 
     public static final String FIELD_COACH_NAME = "com.bizfit.field.name";
     public static final String FIELD_COACH_IMAGE_ID = "com.bizfit.field.image";
     private static final int IMAGE_NOT_FOUND = -1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coach_page);
         ExpandableTextView expTv1 = (ExpandableTextView) findViewById(R.id.expand_text_view)
@@ -30,12 +36,20 @@ public class CoachPage extends AppCompatActivity {
         expTv1.setText(getString(R.string.dummy));
         Intent intent = getIntent();
 
-        if (intent != null) {
+        if (intent != null)
+        {
             String name = intent.getStringExtra(FIELD_COACH_NAME);
             int imgId = intent.getIntExtra(FIELD_COACH_IMAGE_ID, IMAGE_NOT_FOUND);
 
+
             if (imgId != IMAGE_NOT_FOUND)
-                ((ImageView) findViewById(R.id.coach_banner_image)).setImageDrawable(getResources().getDrawable(imgId));
+            {
+                //  Fixed by JariJ 25.11.16
+                ((ImageView) findViewById(R.id.coach_banner_image)).setImageDrawable(ContextCompat.getDrawable(this, imgId));
+            }
+            /** Deprecated function getResources()
+            // ((ImageView) findViewById(R.id.coach_banner_image)).setImageDrawable(getResources().getDrawable(imgId));
+             */
             if (name != null)
                 ((TextView) findViewById(R.id.textView5)).setText(name);
         }
