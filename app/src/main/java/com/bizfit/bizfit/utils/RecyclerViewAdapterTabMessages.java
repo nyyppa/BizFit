a24a3f09a25df6448949f3f2989d92bca70252ed
JJarvenpaa <jari.jarvenpaa@hotmail.com>
2016-12-05 13:52:06 +0200

    utils->  RecyclerViewAdapterTabMessages.java: Recipient picture drawing added layout -> list_item_message_tab_preview.xml: Recipient picture showing

--- app/src/main/java/com/bizfit/bizfit/utils/RecyclerViewAdapterTabMessages.java
+++ app/src/main/java/com/bizfit/bizfit/utils/RecyclerViewAdapterTabMessages.java
@@ -1,11 +1,14 @@
 package com.bizfit.bizfit.utils;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.bizfit.bizfit.R;
 import com.bizfit.bizfit.activities.MessageActivity;
@@ -13,11 +16,16 @@ import com.bizfit.bizfit.activities.MessageActivity;
 /**
  *
  */
-public class RecyclerViewAdapterTabMessages extends RecyclerView.Adapter {
+public class RecyclerViewAdapterTabMessages extends RecyclerView.Adapter
+{
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.list_item_message_tab_preview, parent, false);
+                 ImageView iVResipient=(ImageView)v.findViewById(R.id.iVRecipient);
+                 iVResipient.setImageDrawable(ContextCompat.getDrawable(parent.getContext(), R.drawable.tmp2));
+
         return new ViewHolder(v);
     }
 
