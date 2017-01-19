3f17b62bb304b3d3db912b52947f4c3b64968a56
JJarvenpaa <jari.jarvenpaa@hotmail.com>
2017-01-19 13:19:03 +0200

    - Added a todo "Tee tämä"

--- app/src/main/java/com/bizfit/bizfit/utils/RecyclerViewAdapterTabMessages.java
+++ app/src/main/java/com/bizfit/bizfit/utils/RecyclerViewAdapterTabMessages.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bizfit.bizfit.R;
+import com.bizfit.bizfit.User;
 import com.bizfit.bizfit.activities.MessageActivity;
 
 /**
@@ -18,12 +19,19 @@ import com.bizfit.bizfit.activities.MessageActivity;
  */
 public class RecyclerViewAdapterTabMessages extends RecyclerView.Adapter
 {
+    //private User user;
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
     {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.list_item_message_tab_preview, parent, false);
                  ImageView iVResipient=(ImageView)v.findViewById(R.id.iVRecipient);
+                //TODO: Tee tämä!
+                //hae kirjautuneen käyttäjän keskustelut (jos tarvii)
+                //lisää keskustelut listaan, hae keskustelulle other ja vertaa sitä owneriin
+                // jos näin, niin luodaan siitä muuttuja ja haetaan sille coachID:n perusteella tiedot
+                //ulosta tiedot
+
                  iVResipient.setImageDrawable(ContextCompat.getDrawable(parent.getContext(), R.drawable.tmp2));
 
         return new ViewHolder(v);
