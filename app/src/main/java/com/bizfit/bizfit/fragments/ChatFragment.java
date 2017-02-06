7a8beeca5bec4c12ce112fcd80990446eaf8970c
iipa <iipa.korhonen@gmail.com>
2017-02-06 11:44:24 +0200

    ui trackerin valintaan

--- app/src/main/java/com/bizfit/bizfit/fragments/ChatFragment.java
+++ app/src/main/java/com/bizfit/bizfit/fragments/ChatFragment.java
@@ -1,17 +1,35 @@
 package com.bizfit.bizfit.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.provider.Telephony;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.bizfit.bizfit.R;
 import com.bizfit.bizfit.User;
+import com.bizfit.bizfit.tracker.Tracker;
 import com.bizfit.bizfit.RecyclerViews.RecyclerViewAdapterMessages;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -84,13 +102,84 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
                 break;
             case R.id.shareTracker:
                 if(User.getLastUser(null,null,null).getTrackers().length>0){
-                    mAdapter.getConversation().createMessage("code share_tracker"+User.getLastUser(null,null,null).getTrackers()[0].shareTracker(mAdapter.getConversation().getOwner()).toString());
-                    mAdapter.notifyItemInserted(0);
-                    mRecyclerView.smoothScrollToPosition(0);
+
+                    shareTrackers();
+
+                } else {
+                    // Notifies user if no trackers are found
+                    Snackbar.make(v.findViewById(R.id.coordinatorMessages), "You don't have any trackers to share", Snackbar.LENGTH_LONG).show();
                 }
 
                 break;
         }
+    }
+
+    /**
+     *  Method for asking user which trackers to share, and sharing them.
+     */
+
+    public void shareTrackers() {
+
+        // Inflating xml-layout into a view
+
+        LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.fragment_select_tracker, null);
+
+        // Adding trackers into a list
+
+        ListView lv = (ListView) layout.findViewById(R.id.share_tracker_listview);
+        Tracker [] trackerarray = User.getLastUser(null, null, null).getTrackers();
+
+        List<String> trackerlist = new ArrayList();
+
+
+        for(int i = 0; i < trackerarray.length; i++) {
+            trackerlist.add(trackerarray[i].getName());
+        }
+
+        // Setting the tracker list into listview
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_multiple_choice, trackerlist);
+        lv.setAdapter(adapter);
+        lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        // Setting up popup attributes
+
+        final PopupWindow popupSelect = new PopupWindow(getActivity());
+        popupSelect.setContentView(layout);
+        popupSelect.setWidth(LinearLayout.LayoutParams.WRAP_CONTENT);
+        popupSelect.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
+        popupSelect.setFocusable(true);
+        popupSelect.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        // Showing popup
+
+        popupSelect.showAtLocation(layout, Gravity.CENTER, 0, 0);
+
+        // Cancel-button listener
+
+        Button close = (Button) layout.findViewById(R.id.tracker_sharing_cancel);
+        close.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                popupSelect.dismiss();
+            }
+        });
+
+        // Share-button listener
+
+        Button share = (Button) layout.findViewById(R.id.tracker_sharing_share);
+        close.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO: share selected trackers
+                mAdapter.getConversation().createMessage("code share_tracker"+User.getLastUser(null,null,null).getTrackers()[0].shareTracker(mAdapter.getConversation().getOwner()).toString());
+                mAdapter.notifyItemInserted(0);
+                mRecyclerView.smoothScrollToPosition(0);
+            }
+        });
     }
 
     public RecyclerViewAdapterMessages getmAdapter()
