cedc789d38e7445f5e1c28100505617229c8447f
nyyppa <atte.yliverronen@gmail.com>
2017-05-15 14:30:25 +0300

    -removed multidex support

--- app/src/main/java/com/bizfit/bizfit/MyApplication.java
+++ app/src/main/java/com/bizfit/bizfit/MyApplication.java
@@ -4,7 +4,6 @@ import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Context;
 import android.os.Build;
-import android.support.multidex.MultiDex;
 
 import org.acra.*;
 import org.acra.annotation.*;
@@ -39,7 +38,6 @@ public class MyApplication extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         baseContext=base;
-        MultiDex.install(this);
         // The following line triggers the initialization of ACRA
         ACRA.init(this);
     }
