83c0962069defd7037f993319e6c5c5f5897d261
nyyppa <atte.yliverronen@gmail.com>
2017-07-11 17:38:07 +0300

    -changed fileUpload to use MyConnection

---
+++ app/src/main/java/com/bizfit/bizfitUusYritysKeskusAlpha/activities/KuvaTestiActivity.java
@@ -0,0 +1,47 @@
+package com.bizfit.bizfitUusYritysKeskusAlpha.activities;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bizfit.bizfitUusYritysKeskusAlpha.R;
+import com.bizfit.bizfitUusYritysKeskusAlpha.network.FileDownload.DrawableDownloader;
+import com.bizfit.bizfitUusYritysKeskusAlpha.network.FileUpload.DrawableUploader;
+import com.bizfit.bizfitUusYritysKeskusAlpha.network.FileUpload.FileUpload;
+import com.bizfit.bizfitUusYritysKeskusAlpha.utils.Constants;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class KuvaTestiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kuva_testi);
+        /*URL[] urls=new URL[1];
+        try {
+            urls[0]=new URL(Constants.connection_address);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        new DrawableDownloader() {
+            @Override
+            public void doResult(Drawable result) {
+                ImageView imageView=(ImageView)KuvaTestiActivity.this.findViewById(R.id.kuva);
+                imageView.setImageDrawable(result);
+            }
+        }.execute(urls);
+
+        */
+        ImageView imageView=(ImageView)findViewById(R.id.kuva);
+        new DrawableUploader() {
+            @Override
+            protected void onPostExecute(FileUpload.ResultCode result) {
+
+            }
+        }.execute(new Drawable[]{imageView.getDrawable()});
+    }
+
+}
